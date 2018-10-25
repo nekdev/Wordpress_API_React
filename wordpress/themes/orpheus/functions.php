55a1f3a4d1c32025d808f6caa99ef1580a0fa085
nekdev <41330892+nekdev@users.noreply.github.com>
2018-10-25 17:35:44 +0300

    Update functions.php

--- wordpress/themes/orpheus/functions.php
+++ wordpress/themes/orpheus/functions.php
@@ -6,7 +6,7 @@ require_once 'inc/Admin.php';
 require_once 'inc/class-acf-commands.php';
 function my_acf_init() {
 	
-	acf_update_setting('google_api_key', 'AIzaSyD8zqgP38IwQeV0zpKiCyp1SOSspjlhnSM');
+	acf_update_setting('google_api_key', '####');
 }
 
 add_action('acf/init', 'my_acf_init');
